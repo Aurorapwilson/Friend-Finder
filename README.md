ca0064511bbf4cbb53fd15ef3bb669e3933cf7fe
Princess Wilson <aurorapwilson1@gmail.com>
2019-02-22 23:45:08 -0500

    friends

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Friend-Finder
 heroku deployed express node app
+
+<!-- * Create a folder called `FriendFinder`. Inside the folder, organize your directories so it matches the following:
+
+  ```
+  FriendFinder
+    - .gitignore
+    - app
+      - data
+        - friends.js
+      - public
+        - home.html
+        - survey.html
+      - routing
+        - apiRoutes.js
+        - htmlRoutes.js
+    - node_modules
+    - package.json
+    - server.js
+  ``` -->
+
+  
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+//Dependencies
+//===============================================================
+const path = require('path');
+const express = require ('express');
+
+//Set up App
+//===============================================================
 const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.listen(PORT, function () {
-
-});
+//this helps heroku server listen to the right port
 app.get("/", function (req, res) {
     res.json(path.join(__dirname, "public/index.html"));
 });
+app.listen(PORT['app_port']);
+
+//test code
+//==================================================================================
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, function () {
+
+// });
+// app.get("/", function (req, res) {
+//     res.json(path.join(__dirname, "public/index.html"));
+// });
