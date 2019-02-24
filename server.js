4ef74b4925695223bc1f401fec4ae995803977a8
Princess Wilson <aurorapwilson1@gmail.com>
2019-02-24 16:24:04 -0500

    server.js bodyparser npm doc

--- server.js
+++ server.js
@@ -2,21 +2,37 @@
 //===============================================================
 const path = require('path');
 const express = require('express');
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
 
 //Set up App
 //===============================================================
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-//this helps heroku server listen to the right port
+
+//setting heroku server
 app.get("/", function (req, res) {
     res.json(path.join(__dirname, "public/index.html"));
 });
+
+
+//Body Parser
+// =============================================================
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+});
+
 app.listen(PORT['app_port'], function () {
     console.log("App is running: " + PORT)
 });
-
 
 //test code
 //==================================================================================
