215a629cb62b132e3a38ee2a254fcd2e0f971dae
Princess Wilson <aurorapwilson1@gmail.com>
2019-02-25 00:59:57 -0500

    chosen library to survery.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@
 // ROUTING
 // ===============================================================================
 //require module exports
-var friends = ("./friends.js")
+var friends = require ("../data/friends.js")
 
 
 //Api Get Requests
@@ -12,11 +12,12 @@ var friends = ("./friends.js")
 //// -------------------------------------------------------------------------------
 
 app.get("/", function(req, res) {
-    res.json(tableData);
+    res.json(friendsArray);
   });
 
+  
   app.get("/", function(req, res) {
-    res.json(waitListData);
+    res.json(friendsArray);
   });
 
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
@@ -25,12 +26,12 @@ app.post("/", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+    if (friendsArray.length < 5) {
+      friendsArray.push(req.body);
       res.json(true);
     }
     else {
-      waitListData.push(req.body);
+      friendsArray.push(req.body);
       res.json(false);
     }
   });
--- server.js
+++ server.js
@@ -1,47 +1,52 @@
-//Dependencies
-//===============================================================
+// //Dependencies
+// //===============================================================
+// const path = require('path');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// //Set up App
+// //===============================================================
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+
+// //setting heroku server
+// app.get("/", function (req, res) {
+//     res.json(path.join(__dirname, "public/index.html"));
+// });
+
+
+// //Body Parser
+// // =============================================================
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+// // parse application/json
+// app.use(bodyParser.json())
+
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// });
+
+// app.listen(PORT['app_port'], function () {
+//     console.log("App is running: " + PORT)
+// });
+
+
+// //Dependencies
+// //===============================================================
+const express = require("express");
 const path = require('path');
-const express = require('express');
 const bodyParser = require('body-parser');
-
-//Set up App
-//===============================================================
 const app = express();
-const PORT = process.env.PORT || 3308;
+const port = 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+app.get('/', (req, res) => res.send("Hallo"));
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
-
-
-//setting heroku server
-app.get("/", function (req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-  
-});
-
-
-//Body Parser
-// =============================================================
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-});
-
-app.listen(PORT['app_port'], function () {
-    console.log("App is running: " + PORT)
-});
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
